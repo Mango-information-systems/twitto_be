db63ed2fb30b4aedcd6724adb5678f4782d5f05b
11 Digit Labs <labs@11dig.it>
2013-06-12 19:33:07 +0300

    Homecontroller was causing error 500 Most probably because it was trying to load ALL users per category and dying.

--- site/app/controllers/HomeController.php
+++ site/app/controllers/HomeController.php
@@ -11,12 +11,15 @@ class HomeController extends BaseController {
         $page_title = 'Home';
         $categorySlug = -1;
 
-		$categories = Category::with('twusers')->orderBy('sorting_order')->get();
+		//$categories = Category::with('twusers')->orderBy('sorting_order')->get();
+		// Above was causing error 500. Investigate!
+		$categories = DB::table('category')->orderBy('sorting_order')->get();
 
 		// Dirty walkaround. I dont know what to do to limit the result while
 		// using eager loading.
 		foreach ($categories as $category){
-			$users[$category->category_id] = $category->twusers()->take(5)->get();
+			$_category = Category::where('category_id', '=', $category->category_id)->first();
+			$users[$category->category_id] = $_category->twusers()->take(5)->get();
 		}
 
 		return View::make('home', compact('page_title', 'categories', 'categorySlug', 'users'));
