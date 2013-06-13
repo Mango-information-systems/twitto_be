d3dbd7fc088b536d2eb95f41bc7b5704ba2cd36d
11 Digit Labs <labs@11dig.it>
2013-06-13 13:56:43 +0300

    Google analytics

--- site/app/views/_layouts/default.blade.php
+++ site/app/views/_layouts/default.blade.php
@@ -89,7 +89,15 @@
 		<!-- Placed at the end of the document so the pages load faster -->
 		<script src="{{ URL::asset('assets/js/jquery-2.0.2.min.js') }}"></script>
 		<script src="{{ URL::asset('assets/js/jquery.isotope.min.js') }}"></script>
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+			ga('create', 'UA-25766439-4', 'twitto.be');
+			ga('send', 'pageview');
+		</script>
 
 
         <script lang="text/javascript">
