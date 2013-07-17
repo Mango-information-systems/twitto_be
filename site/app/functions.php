a7fb6453a2c200733fb1dcda3278377bf5a5de48
11 Digit Labs <labs@11dig.it>
2013-07-17 13:01:54 +0300

    #35/ Parse description , replace #, @ and urls with HTML rather than plain text

--- site/app/functions.php
+++ site/app/functions.php
@@ -6,10 +6,17 @@
 |--------------------------------------------------------------------------
 |
 */
-function twitter_txt_parse($txt){
-	$txt = preg_replace('/((http)+(s)?:\/\/[^<>\s]+)/iu', '<a href="$0" target="_blank">$0</a>', $txt );
-	$txt = preg_replace('/[@]+(\w+)/u', '<a href="https://twitter.com/$1" target="_blank">$0</a>', $txt );
-	$txt = preg_replace('/[#]+(\w+)/u', '<a href="https://twitter.com/search?q=%23$1" target="_blank">$0</a>', $txt );
 
-	return $txt;
+//https://github.com/mzsanford/twitter-text-php
+function twitter_txt_parse($txt){
+
+	require_once app_path().'/lib/TwitterText/Autolink.php';
+	require_once app_path().'/lib/TwitterText/Extractor.php';
+	require_once app_path().'/lib/TwitterText/HitHighlighter.php';
+
+	$html = Twitter_Autolink::create($txt)
+		->setNoFollow(false)
+		->addLinks();
+
+	return $html;
 }
