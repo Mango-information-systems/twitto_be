e7b1c2fdfc4400c621d1b3673f058898a0b4ab82
Mehdi El Fadil <projects@mango-is.com>
2013-08-08 16:21:11 +0200

    added processes to update person's info from twitter

--- data-processing/l-tw-user.js
+++ data-processing/l-tw-user.js
@@ -1,4 +1,4 @@
-// update of twitter users table based on refreshed staging content
+// load tw_user based on refreshed staging content
 var Mysql = require('mysql')
 	, params = require('./params.json')
 	, processDate = new Date()
@@ -72,28 +72,57 @@ function timeStampToMysqlFormat(date1) {
 
 function loadUsers() {
 // update production table
+// uses etl audit table to select only records updated since last successful etl run
 
 	var qry = mysql.query('INSERT INTO tw_user (tw_id, screen_name, name, description, profile_image_url, lang) SELECT id, screen_name, name, description, profile_image_url, lang from stg_tw_user WHERE deleted = 0 and last_update > (SELECT last_run from audit_etl where process_name = "load_tw_user") ON DUPLICATE KEY UPDATE screen_name = VALUES(screen_name), name = VALUES(name), description = VALUES(description), profile_image_url = VALUES(profile_image_url), lang = VALUES(lang)', function(err, data) {
 		if (err) {
-			console.log('error storing results for ',data)
+			console.log('error updating users', data)
 			throw err
 		}
 		else {
-			console.log('results saved , affected rows:', data.affectedRows)
-			// update audit date
-			mysql.query('update audit_etl set last_run = "' + timeStampToMysqlFormat(processDate) + '" where process_name = "load_tw_user"', function(err, data) {
+			console.log('rows updated , affected rows:', data.affectedRows)
+			// remove users deleted from twitter
+			var qry = mysql.query('DELETE FROM tw_user where tw_id in (SELECT id from stg_tw_user WHERE deleted = 1 and last_update > (SELECT last_run from audit_etl where process_name = "load_tw_user"))', function(err, data) {
 				if (err) {
-					console.log('error updating etl audit table with process run date',data)
+					console.log('error removing users', data)
 					throw err
 				}
 				else {
-					console.log('etl audit table timestamp updated for the current process, exiting...')
-					shutdownProcess()
+					console.log('deleted users, affected rows:', data.affectedRows)
+					// cleanup fact topics
+					var qry = mysql.query('DELETE FROM fact_topic where tw_id in (SELECT id from stg_tw_user WHERE deleted = 1 and last_update > (SELECT last_run from audit_etl where process_name = "load_tw_user"))', function(err, data) {
+						if (err) {
+							console.log('error deleting old users from fact_topic', data)
+							throw err
+						}
+						else {
+							console.log('cleaned up topics, affected rows:', data.affectedRows)
+							// update audit date
+							mysql.query('UPDATE audit_etl SET last_run = "' + timeStampToMysqlFormat(processDate) + '" WHERE process_name = "load_tw_user"', function(err, data) {
+								if (err) {
+									console.log('error updating etl audit table with process run date',data)
+									throw err
+								}
+								else {
+									console.log('etl audit table timestamp updated for the current process, exiting...')
+									shutdownProcess()
+								}
+							})
+						}
+					})
+					/*	console.log('-------------------------------------------------------------------')
+						console.log('rendered query', qry.sql)
+						console.log('-------------------------------------------------------------------')
+					*/
 				}
 			})
+			/*	console.log('-------------------------------------------------------------------')
+				console.log('rendered query', qry.sql)
+				console.log('-------------------------------------------------------------------')
+			*/
 		}
 	})
-	/*
+
 /*	console.log('-------------------------------------------------------------------')
  	console.log('rendered query', qry.sql)
 	console.log('-------------------------------------------------------------------')
