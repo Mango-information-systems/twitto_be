e162b88b66bab8618d1d4dbcc34208c66a135168
Mehdi El Fadil <m.ef@mango-is.com>
2013-08-18 10:16:04 +0200

    #7 removed 'processing...' indicator from SDOM was not activated anyway (would require bprocessing: true option)

--- site/app/views/home.blade.php
+++ site/app/views/home.blade.php
@@ -349,7 +349,7 @@ d3.json('json/users.json', function (data) {
 		}
 		
 		dataTable = $('#twitter-datatable').dataTable( {
-			"sDom": "<'row-fluid'<'span6'T>r>t<'row-fluid'<'span6 pull-right'p>",
+			"sDom": "t<'row-fluid'<'span6 pull-right'p>",
 			"sAjaxDataProp": "",
 			"bDeferRender": true, //speed  http://datatables.net/ref#bDeferRender
 			"aaData": [	],
