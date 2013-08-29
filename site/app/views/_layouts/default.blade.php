ed298f31e37397887d0eceb7de8929841fa506ef
Mehdi El Fadil <m.ef@mango-is.com>
2013-08-29 19:13:28 +0200

    added exceptions tracking to Google Analytics, closes #21 based on https://developers.google.com/analytics/devguides/collection/analyticsjs/events

--- site/app/views/_layouts/default.blade.php
+++ site/app/views/_layouts/default.blade.php
@@ -145,10 +145,14 @@
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga')
 
-	ga('create', 'UA-25766439-4', 'twitto.be');
-	ga('send', 'pageview');
+	ga('create', 'UA-25766439-4', 'twitto.be')
+	ga('send', 'pageview')
+	
+	window.onerror = function(message, file, line) {
+		ga('send', 'event', 'Exceptions', file + ' - ' + line, message)
+	}
 </script>
 @endif
 
