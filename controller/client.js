28b9f624f50c99e84a0397adef4c6388593e2977
11 Digit Labs <labs@11dig.it>
2016-09-02 11:50:23 +0300

    #85 - Fixed Nan - All progress bars should be updated

--- controller/client.js
+++ controller/client.js
@@ -9,10 +9,6 @@ var suffix = ':3030'
 	, svg = d3.select('#mapContainer')
 	, mapRenderer = new MapRenderer(svg, tweetsCache)
 	, statsCalculator = new StatsCalculator(tweetsCache)
-	, $statsTotalTweetsLabel = d3.select('#stats .total-tweets .label')
-	, $statsTotalTweetsProgress = d3.select('#stats .total-tweets progress')
-	, $statsOriginalTweetsLabel = d3.select('#stats .original-tweets .label')
-	, $statsOriginalTweetsProgress = d3.select('#stats .original-tweets progress')
 	, stats = statsCalculator.calculate()
 
 app.socket = io(window.location.hostname + suffix, {path: '/ws/'})
@@ -20,41 +16,12 @@ app.socket = io(window.location.hostname + suffix, {path: '/ws/'})
 app.socket.on('tweet', function (msg) {
 	//~ console.log('new tweet received', msg.id_str)
 	mapRenderer.updatePoints(msg)
-	stats.total++
-	$statsTotalTweetsLabel.html(stats.total)
+	stats = statsCalculator.increment(stats, msg)
+	statsCalculator.render(stats, msg)
 
-	if(msg.in_reply_to_user_id == null && msg.retweeted == false) {
-		stats.original++
-		$statsOriginalTweetsLabel.html(stats.original)
-
-		$statsOriginalTweetsProgress
-			.transition()
-			.delay(function (d, i) {
-				return i * 200
-			})
-			.duration(1000)
-			.attr('value', (stats.original * 100) / stats.total)
-
-	}
 })
 
-$statsTotalTweetsLabel.html(stats.total)
-$statsTotalTweetsProgress
-	.transition()
-	.delay(function (d, i) {
-		return i * 200
-	})
-	.duration(1000)
-	.attr('value', 100)
-
-$statsOriginalTweetsLabel.html(stats.original)
-$statsOriginalTweetsProgress
-	.transition()
-	.delay(function (d, i) {
-		return i * 200
-	})
-	.duration(1000)
-	.attr('value', (stats.original * 100) / stats.total)
+statsCalculator.render(stats, {})
 
 
 
