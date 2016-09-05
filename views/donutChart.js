5c7f31b8c5977e5f11b0de628bb7015d5959bb38
Mehdi El Fadil <m.ef@mango-is.com>
2016-09-05 20:13:15 +0200

    wip in-chart legend #85 TODO move this and the static donut into index.ejs

--- views/donutChart.js
+++ views/donutChart.js
@@ -37,6 +37,13 @@ function DonutChart(svg) {
 		.datum({endAngle: 0.001 * tau})
 		.style('fill', '#008000')
 		.attr('d', arc)
+		
+	this.g.append('line')
+		.attr('x1', 50)
+		.attr('x2', 150)
+		.attr('y1', -140)
+		.attr('y2', -140)
+		.style('stroke', '#999999')
 	
 	/****************************************
 	 *
