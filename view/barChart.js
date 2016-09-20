ff92926858e7a2db2d8bdbfd4d461dcea77d25ab
11 Digit Labs <labs@11dig.it>
2016-09-20 19:43:29 +0300

    #100 - works but still WIP!!!

--- view/barChart.js
+++ view/barChart.js
@@ -15,11 +15,20 @@ function BarChart (svg) {
 		, svgWidth = 450
 
 	
-	var margin = {top: 20, right: 20, bottom: 80, left: 80}
+	var margin = {top: 0, right: 20, bottom: 80, left: 0}
 		, width = svgWidth - margin.left - margin.right
 		, height = 300 - margin.top - margin.bottom
 		, categoryIndent = 4 * 15 + 5
-		, defaultBarWidth = 2000
+		, defaultBarWidth = 3000
+
+	var x = d3.scaleLinear()
+		.domain([0, defaultBarWidth])
+		.range([0, width])
+
+	var y = d3.scaleBand()
+		.range([0, height])
+		.padding(0.1)
+		.round(0)
 
 	var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
@@ -43,10 +52,144 @@ function BarChart (svg) {
 	 * @param {object} timeline time series data
 	 *
 	 ************/
-	this.init = function (allEntitiesStats, topEntitiesStats, lowestcount) {
-		this.topEntitiesStats = topEntitiesStats
+	this.init = function (allEntitiesStats, topEntitiesStats, lowestCount) {
+		self.allEntitiesStats = allEntitiesStats
+		self.topEntitiesStats = topEntitiesStats
+		self.lowestCount = lowestCount
 
-		console.log(allEntitiesStats, topEntitiesStats, lowestcount)
+		console.log(allEntitiesStats, topEntitiesStats, lowestCount)
+
+		setInterval(function () {
+			self.redrawChart()
+		}, 3000);
+	}
+
+	this.redrawChart = function(){
+
+		//Reset domains
+		y.domain(self.topEntitiesStats.sort(function (a, b) {
+				return b.value - a.value
+			})
+			.map(function (d) {
+				return d.key
+			}))
+		var barmax = d3.max(self.topEntitiesStats, function (e) {
+			return e.value
+		})
+		x.domain([0, barmax])
+
+
+		/////////
+		//ENTER//
+		/////////
+
+		//Bind new data to chart rows
+
+		//Create chart row and move to below the bottom of the chart
+		var chartRow = g.selectAll("g.chartRow")
+			.data(self.topEntitiesStats, function (d) {
+				return d.key
+			})
+
+		var yposition = height + margin.top + margin.bottom;
+
+		var newRow = chartRow
+			.enter()
+			.append("g")
+			.attr("class", "chartRow")
+			.attr("transform", "translate(0," + yposition + ")")
+
+		//Add rectangles
+		newRow.insert("rect")
+			.attr("class", "bar")
+			.attr("x", 0)
+			.attr("opacity", 0)
+			.attr("height", y.bandwidth() * 2)
+			.attr("width", function (d) {
+				return x(d.value)
+			})
+
+		//Add value labels
+		newRow.append("text")
+			.attr("class", "label")
+			.attr("y", y.bandwidth() / 2)
+			.attr("x", 0)
+			.attr("opacity", 0)
+			.attr("dy", ".35em")
+			.attr("dx", "0.5em")
+			.text(function (d) {
+				return d.value
+			})
+
+		//Add Headlines
+		newRow.append("text")
+			.attr("class", "category")
+			.attr("text-overflow", "ellipsis")
+			.attr("y", y.bandwidth() / 2)
+			.attr("x", categoryIndent)
+			.attr("opacity", 0)
+			.attr("dy", ".35em")
+			.attr("dx", "0.5em")
+			.text(function (d) {
+				return d.key
+			})
+
+		//////////
+		//UPDATE//
+		//////////
+
+		//Update bar widths
+		chartRow.select(".bar").transition()
+			.duration(300)
+			.attr("width", function (d) {
+				return x(d.value)
+			})
+			.attr("opacity", 1)
+
+		//Update data labels
+		chartRow.select(".label").transition()
+			.duration(300)
+			.attr("opacity", 1)
+			.tween("text", function (d) {
+				var i = d3.interpolate(+this.textContent.replace(/\,/g, ''), +d.value)
+				return function (t) {
+					this.textContent = Math.round(i(t))
+				}
+			})
+
+		//Fade in categories
+		chartRow.select(".category").transition()
+			.duration(300)
+			.attr("opacity", 1)
+
+
+		////////
+		//EXIT//
+		////////
+
+		//Fade out and remove exit elements
+		chartRow.exit().transition()
+			.style("opacity", "0")
+			.attr("transform", "translate(0," + (height + margin.top + margin.bottom) + ")")
+			.remove()
+
+
+		////////////////
+		//REORDER ROWS//
+		////////////////
+
+		var delay = function (d, i) {
+			return 200 + i * 30
+		}
+
+		chartRow.transition()
+			.delay(delay)
+			.duration(900)
+			.attr("transform", function (d) {
+				return "translate(0," + y(d.key) * 2 + ")"
+			})
+
+
 	}
 
 	return this	
