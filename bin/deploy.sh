21b652aae59cb90c53d110a4a986ce0911c92d9f
Mehdi El Fadil <m.ef@mango-is.com>
2020-02-14 17:39:57 +0100

    update deploy script #148
    to be tested.

---
+++ bin/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ -z ${1} ];
+then
+	echo "environment is unset, exiting";
+else
+	echo "deploying to '$1' environment";
+	target=$(if [ "prod" == "$1" ]; then echo "twitto"; else echo "test-twitto"; fi) \
+	&& sudo rsync -ua --exclude="*node_modules/*" --exclude=".*" --exclude="persist/*" ./* /home/srv-node-mango/$target \
+	&& sudo chown -R srv-node-mango:srv-node-mango /home/srv-node-mango/$target \
+	&& cd /home/srv-node-mango/$target
+	&& NODE_ENV=production npm install \
+	&& sudo systemctl restart node-$target \
+	&& sudo touch /var/ngx_pagespeed_cache/cache.flush
+	&& echo "done";
+fi
