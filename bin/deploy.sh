504caa404a233dd729f77fde60197efe3aa26c8f
Mehdi El Fadil <m.ef@mango-is.com>
2020-02-19 21:09:10 +0100

    fix deploy script #148

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,10 +8,10 @@ else
 	echo "deploying to '$1' environment";
 	target=$(if [ "prod" == "$1" ]; then echo "twitto"; else echo "test-twitto"; fi) \
 	&& sudo rsync -ua --exclude="*node_modules/*" --exclude=".*" --exclude="persist/*" ./* /home/srv-node-mango/$target \
-	&& sudo chown -R srv-node-mango:srv-node-mango /home/srv-node-mango/$target \
-	&& cd /home/srv-node-mango/$target
+	&& cd /home/srv-node-mango/$target \
 	&& NODE_ENV=production npm install \
+	&& sudo chown -R srv-node-mango:srv-node-mango /home/srv-node-mango/$target \
 	&& sudo systemctl restart node-$target \
-	&& sudo touch /var/ngx_pagespeed_cache/cache.flush
+	&& sudo touch /var/ngx_pagespeed_cache/cache.flush \
 	&& echo "done";
 fi
