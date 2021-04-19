e10fc02e190dabab2450d09fc5c9dc7d1325dc53
Mehdi El Fadil <m.ef@mango-is.com>
2021-04-19 10:17:04 +0200

    fix permission issue in deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,9 +7,11 @@ then
 else
 	echo "deploying to '$1' environment";
 	target=$(if [ "prod" == "$1" ]; then echo "twitto"; else echo "test-twitto"; fi) \
-	&& sudo rsync -ua --exclude="*node_modules/*" --exclude=".*" --exclude="persist/*" ./* /home/srv-node-mango/$target \
-	&& cd /home/srv-node-mango/$target \
+	&& rm -r node_modules/* \
 	&& NODE_ENV=production npm install \
+	&& npm audit fix \
+	&& sudo rsync -ua --exclude=".*" --exclude="persist/*" ./* /home/srv-node-mango/$target \
+	&& cd /home/srv-node-mango/$target \
 	&& sudo chown -R srv-node-mango:srv-node-mango /home/srv-node-mango/$target \
 	&& sudo systemctl restart node-$target \
 	&& sudo touch /var/ngx_pagespeed_cache/cache.flush \
